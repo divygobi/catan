22b5aa097d6656c7e046d28fa773ce99e82dd79c
Divy Gobiraj <divygobi@gmail.com>
2024-03-06 23:52:51 -0800

    added colors and texture for hexes

--- core/src/com/mygdx/catan/Hex.java
+++ core/src/com/mygdx/catan/Hex.java
@@ -1,5 +1,10 @@
 package com.mygdx.catan;
 
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.PolygonSprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
 import java.util.Arrays;
 import java.util.Random;
 
@@ -10,18 +15,52 @@ public class Hex {
     private int value;
     private int[][] vertices;
     private int[] axialCoord;
+    private com.badlogic.gdx.graphics.g2d.TextureRegion textureRegion ;
 
     public Hex(String type, int value, int[] axialCoord) {
         this.resourceType = type;
         this.axialCoord = axialCoord;
         this.value = value;
+        Pixmap pix = new Pixmap(1,1,Pixmap.Format.RGB888 );
 
+
+        switch (resourceType){
+            case "WOOD":
+                pix.setColor(0.76f, 0.6f, 0.42f, 1); // Using a brown color
+            case "WHEAT":
+                pix.setColor(0.93f, 0.9f, 0.55f, 1); // A light, golden brown
+            case "ROCK":
+                pix.setColor(0.5f, 0.5f, 0.5f, 1); // Medium gray
+            case "CLAY":
+                pix.setColor(0.72f, 0.45f, 0.2f, 1); // A reddish-brown
+            case "SHEEP":
+                pix.setColor(0.6f, 0.8f, 0.4f, 1); // Light green
+        }
+        this.textureRegion = getTextureRegion(this.resourceType);
     }
 
     public Hex(int[] axialCoord){
         this.axialCoord = axialCoord;
         this.resourceType = getRandResource();
         this.value = getRandValue();
+        this.textureRegion = getTextureRegion(this.resourceType);
+    }
+
+    private TextureRegion getTextureRegion(String resourceType) {
+        Pixmap pix = new Pixmap(1,1,Pixmap.Format.RGB888 );
+        switch (resourceType){
+            case "WOOD":
+                pix.setColor(0.76f, 0.6f, 0.42f, 1); // Using a brown color
+            case "WHEAT":
+                pix.setColor(0.93f, 0.9f, 0.55f, 1); // A light, golden brown
+            case "ROCK":
+                pix.setColor(0.5f, 0.5f, 0.5f, 1); // Medium gray
+            case "CLAY":
+                pix.setColor(0.72f, 0.45f, 0.2f, 1); // A reddish-brown
+            case "SHEEP":
+                pix.setColor(0.6f, 0.8f, 0.4f, 1); // Light green
+        }
+        return new TextureRegion(new Texture(pix));
     }
 
     private int getRandValue(){
@@ -77,5 +116,13 @@ public class Hex {
                 ", vertices=" + Arrays.toString(vertices) +
                 ", axialCoord=" + Arrays.toString(axialCoord) +
                 '}';
+    }
+
+    public TextureRegion getTextureRegion() {
+        return textureRegion;
+    }
+
+    public void setTextureRegion(TextureRegion textureRegion) {
+        this.textureRegion = textureRegion;
     }
 }
